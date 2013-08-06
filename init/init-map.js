f32758df2a4026ffb45345ac24cf47589e459bae
Jacob Clark <jacob.jh.clark@googlemail.com>
2013-08-06 11:10:38 +0100

    Deleted files

--- init/init-map.js
+++
@@ -1,8 +0,0 @@
-var mapOptions = {
-	center: new google.maps.LatLng(53.483282, -2.254772),
-	zoom: 5,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-
-var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
